63450c0d03d13c06b1897a6b8f038bcf89c6f6be
116495254+MIbrahim71@users.noreply.github.com <Ibrahim_M789@outlook.com>
2024-09-16 23:58:20 +0100

    home

--- src/components/HomePage/WorkoutFrequency.jsx
+++ src/components/HomePage/WorkoutFrequency.jsx
@@ -1,9 +1,10 @@
 export default function WorkoutFrequency() {
   return (
-    <div className="flex flex-col w-[100%] py-8 border rounded-md cursor-pointer">
-      <div className="flex items-center justify-center">
+    <div className="flex flex-col w-[100%] border rounded-md cursor-pointer">
+      <div className="flex p-1">
         <h1 className="">Workout Frequency</h1>
       </div>
+      <div className="flex justify-center p-2">DAYS OF THE WEEK</div>
     </div>
   );
 }
