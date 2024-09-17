f4b14a830d19d45a0d890b52f1d7becc7c3ca256
116495254+MIbrahim71@users.noreply.github.com <Ibrahim_M789@outlook.com>
2024-09-17 13:11:52 +0100

    basic routing

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,24 @@ import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import Home from "./components/HomePage/Home";
+import Layout from "./components/Layout";
 import MyAccount from "./components/MyAccount/MyAccount";
 import MyWorkouts from "./components/MyWorkouts/myWorkouts";
-import Navbar from "./components/Navbar/navbar";
 
 const router = createBrowserRouter([
-  { path: "/", element: <Home /> },
-  { path: "/MyWorkouts", element: <MyWorkouts /> },
-  { path: "/MyAccount", element: <MyAccount /> },
+  {
+    path: "/",
+    element: <Layout />, // The Layout wraps the Navbar and route content
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/MyWorkouts", element: <MyWorkouts /> },
+      { path: "/MyAccount", element: <MyAccount /> },
+    ],
+  },
 ]);
 
 function App() {
-  return (
-    <div className="flex flex-col max-w-[700px] h-screen items-center m-auto relative">
-      <RouterProvider router={router} />
-      <div className="absolute bottom-0 left-0 w-[100%]">
-        <Navbar />
-      </div>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,20 +1,27 @@
-import React from "react";
+import * as React from "react";
+import { NavLink } from "react-router-dom";
 
 import { homeIcon, dumbellIcon, userIcon } from "../../assets/assets";
 export default function Navbar() {
   return (
-    <div className="flex flex-row items-center justify-center border rounded-md mt-4 bg-slate-50">
+    <div className="flex flex-row border justify-center rounded-md mt-4 bg-slate-50">
       <div className="flex flex-row items-center justify-between gap-4 w-[80%] ">
-        <a className="cursor-pointer">
-          <img className="w-[42px] h-42px]" src={homeIcon} alt="" />
-        </a>
-
-        <a className="cursor-pointer">
-          <img className="w-[72px] h-[72px]" src={dumbellIcon} alt="" />
-        </a>
-        <a className="cursor-pointer">
-          <img className="w-[42px] h-[42px]" src={userIcon} alt="" />
-        </a>
+        <NavLink to="/">
+          <a className="cursor-pointer">
+            <img className="w-[42px] h-42px]" src={homeIcon} alt="" />
+          </a>
+        </NavLink>
+        <NavLink to="MyWorkouts">
+          <a className="cursor-pointer">
+            <img className="w-[72px] h-[72px]" src={dumbellIcon} alt="" />
+          </a>
+        </NavLink>
+        <NavLink to="MyAccount">
+          {" "}
+          <a className="cursor-pointer">
+            <img className="w-[42px] h-[42px]" src={userIcon} alt="" />
+          </a>
+        </NavLink>
       </div>
     </div>
   );
