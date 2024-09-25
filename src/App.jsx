3a01131bf1f5e0424bed3681d712392efcbb453a
116495254+MIbrahim71@users.noreply.github.com <Ibrahim_M789@outlook.com>
2024-09-25 12:09:22 +0100

    app

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./components/HomePage/Home";
 import Layout from "./Layout";
 import MyAccount from "./components/MyAccount/MyAccount";
-import MyWorkouts from "./components/MyWorkouts/MyWorkouts.jsx";
+import MyWorkouts from "./components/MyWorkouts/myWorkouts";
 import WorkoutDetail, {
   workoutLoader,
 } from "./components/MyWorkouts/WorkoutDetail/";
