d91914309cd0448660e9bdf707541959e977dafd
116495254+MIbrahim71@users.noreply.github.com <Ibrahim_M789@outlook.com>
2024-10-17 15:03:52 +0100

    auth

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,27 +1,29 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const authMiddleware = require("../middleware/auth");
 const Workout = require("../models/Workout");
 const router = express.Router();
 
 let workouts = [];
 
 // CREATE a new workout
-router.post("/", async (req, res) => {
-  const { name, exercises, userId } = req.body;
-  if (!name || !exercises || !userId) {
+router.post("/", authMiddleware, async (req, res) => {
+  const { name, exercises } = req.body;
+  if (!name || !exercises) {
     return res.status(400).json({ message: "Missing required fields" });
   }
-  const workout = new Workout({ name, exercises, userId }); // Create new Workout instance
 
   try {
     // Convert userId to ObjectId
-    const validUserId = mongoose.Types.ObjectId.isValid(userId)
-      ? new mongoose.Types.ObjectId(userId)
+    const validUserId = mongoose.Types.ObjectId.isValid(req.user)
+      ? new mongoose.Types.ObjectId(req.user)
       : null;
 
     if (!validUserId) {
       return res.status(400).json({ message: "Invalid userId format" });
     }
+
+    const workout = new Workout({ ...req.body, userId: req.user }); // Create new Workout instance
 
     const newWorkout = await workout.save(); // Save Workout to database
     res.status(201).json(newWorkout); // Respond with new Workout
@@ -47,20 +49,20 @@ router.get("/:id", async (req, res) => {
     if (!workout) return res.status(404).json({ message: "Workout Not Found" });
     res.json(workout);
   } catch (error) {
-    res.status.apply(500).json({ message: error.message });
+    res.status(500).json({ message: error.message });
   }
 });
 
 // UPDATE a workout
 router.put("/:id", async (req, res) => {
   try {
-    const workout = workouts.find(
-      (workout) => workout.id === parseInt(req.params.id)
-    );
+    const workout = await Workout.findById(req.params.id);
     if (!workout) return res.status(404).send("Workout not found");
 
+    // Update workout properties with body data or retain the original
     workout.name = req.body.name || workout.name;
     workout.exercises = req.body.exercises || workout.exercises;
+    // Saves changes to database
     const updatedWorkout = await workout.save();
     res.json(updatedWorkout);
   } catch (error) {
@@ -71,12 +73,10 @@ router.put("/:id", async (req, res) => {
 // DELETE a workout
 router.delete("/:id", async (req, res) => {
   try {
-    const workout = req.params.id;
-    const deletedWorkout = await Workout.findByIdAndDelete(workout);
+    const workout = await Workout.findByIdAndDelete(req.params.id);
+    if (!workout) return res.status(404).send("Workout not found");
 
-    if (!deletedWorkout) return res.status(404).send("Workout not found");
-
-    res.json({ message: "Workout deleted", deletedWorkout });
+    res.json({ message: "Workout deleted", workout });
   } catch (error) {
     res.status(500).json({ message: "Server error", error });
   }
